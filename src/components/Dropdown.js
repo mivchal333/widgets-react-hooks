9eb88d5da6fd72aa726fe59b00369c97fdfd75c3
mivchal <mivchal@gmail.com>
2020-08-27 22:21:51 +0200

    useRef, addEventListener

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
 const Dropdown = ({options, selected, onSelectedChange}) => {
+    const [open, setOpen] = useState(false)
+    const ref = useRef()
+
+    useEffect(() => {
+        document.body.addEventListener('click', event => {
+            if (ref.current.contains(event.target)) {
+                return;
+            }
+            setOpen(false)
+        });
+    }, [])
     const renderedOptions = options.map(option => {
+        if (option.value === selected.value) {
+            return null
+        }
         return (
             <div
                 key={option.value}
@@ -12,15 +26,15 @@ const Dropdown = ({options, selected, onSelectedChange}) => {
             </div>
         )
     })
-    return <div className="ui form">
+    return <div className="ui form" ref={ref}>
         <div className="field">
             <label className="label">
                 Select a colour
             </label>
-            <div className="ui selection dropdown visible active">
+            <div className={`ui selection dropdown ${open ? 'visible active' : ''} `} onClick={() => setOpen(!open)}>
                 <i className="dropdown icon"></i>
                 <div className="text">{selected.label}</div>
-                <div className="menu visible transition">
+                <div className={`menu ${open ? 'visible transition' : ''} `}>
                     {renderedOptions}
                 </div>
             </div>
